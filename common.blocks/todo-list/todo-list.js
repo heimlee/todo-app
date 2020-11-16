019f68db5ff37ebdd3f1e5284ce7570096581add
Andrey Leonets <awdenheimlee@gmail.com>
2020-11-16 22:04:14 +0200

    Add drag and drop

--- common.blocks/todo-list/todo-list.js
+++ common.blocks/todo-list/todo-list.js
@@ -2,36 +2,63 @@ $(function(){
   $(".add-task > button").click(function() {
     $(this).click(function() {
       if($(".input-text").val() != "") {
-        var task = $("<div class='task'></div>").text($(".input-text").val());
+        
+        var $task = $("<div class='task'></div>").text($(".input-text").val());
 
-        var del = $("<span class='ui-icon ui-icon-trash'></span>").click(function() {
-          var p = $(this).parent();
-          p.fadeOut(function() {
-            p.remove();
+        var $del = $("<span class='ui-icon ui-icon-trash'></span>").click(function() {
+          var $p = $(this).parent();
+          $p.fadeOut(function() {
+            $p.remove();
           });
         });
 
-        var check = $("<span class='ui-icon ui-icon-check'></span>").click(function() {
-          var p = $(this).parent();
-          p.fadeOut(function() {
-            $(".completed").append(p);
-            p.fadeIn();
+        var $check = $("<span class='ui-icon ui-icon-check'></span>").click(function() {
+          var $p = $(this).parent();
+          $p.fadeOut(function() {
+            $(".completed").append($p);
+            $p.fadeIn();
           });
           $(this).remove();
         });
 
-        task.append(del, check);
+        $task.append($del, $check);
 
-        $(".incompleted").append(task);
+        $(".incompleted").append($task);
 
         $(".input-text").val("");
 
-        var delBtn = $(".delete-btn").click(function(){
-          task.fadeOut(function() {
-            task.remove();
+        // Drag & Drop
+
+        function droppableTask() {
+          $task.fadeOut(function() {
+            $task.find($check).remove();
+            $(".completed").append($task);
+            $task.fadeIn();
+          });
+        };
+
+        $task.draggable({
+          revert: true,
+          revertDuration: 100,
+          containment: "document"
+        });
+
+        $(".completed").droppable({
+          accept: $task,
+          activeClass: "completed-active",
+          drop: function(event, ui) {
+            droppableTask(ui.draggable);
+          }
+        });
+
+        // Delete button
+
+        var $delBtn = $(".delete-btn").click(function(){
+          $task.fadeOut(function() {
+            $task.remove();
           })
         });
       }
     });
   });
-})
+});
