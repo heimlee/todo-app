c21ff67855ecb6e4ba09856debf1bf62c5b77afd
Andrey Leonets <awdenheimlee@gmail.com>
2021-07-23 19:16:35 +0300

    Style (todoInput): styled todoInput component with makeStyles hook (material-ui)

--- src/containers/todo/components/todoInput/TodoInput.js
+++ src/containers/todo/components/todoInput/TodoInput.js
@@ -1,14 +1,49 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 import { addTask } from '../../actions/todoActions';
 
-import './todoInput.scss';
+const useStyles = makeStyles({
+  root: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: '50px 30px 30px',
+    '& .MuiOutlinedInput-root': {
+      color: '#3f51b5',
+
+      '& fieldset': {
+        border: '2px solid #666'
+      },
+      '&:hover fieldset': {
+        borderColor: '#3f51b5'
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#3f51b5',
+      },
+    },
+  },
+  input: {
+    borderRadius: 6,
+    width: 500,
+  },
+  button: {
+    border: 0,
+    borderRadius: 6,
+    marginLeft: 10,
+    width: 200,
+  },
+});
 
 export const TodoInput = () => {
   const [text, setText] = useState('');
   const dispatch = useDispatch();
   const onTextChange = (e) => setText(e.target.value);
+  const classes = useStyles();
 
   const onSaveTaskClicked = () => {
     if (text) {
@@ -18,21 +53,30 @@ export const TodoInput = () => {
   };
 
   return(
-    <form>
-      <input 
-        type="text" 
-        className=""
+    <FormControl variant="outlined" className={classes.root}>
+      <InputLabel
+        htmlFor="input-new-task"
+        variant="outlined"
+      >
+        New Task
+      </InputLabel>
+      <OutlinedInput 
+        id="input-new-task"
+        className={classes.input}
         placeholder="Write your task"
         value={text}
         onChange={onTextChange}
+        labelWidth={70}
       />
-      <button
+      <Button
+        variant="contained"
         type="button"
-        className=""
+        className={classes.button}
         onClick={onSaveTaskClicked}
+        color="primary"
       >
-        Add
-      </button>
-    </form>
+        Add Task
+      </Button>
+    </FormControl>
   );
 };
